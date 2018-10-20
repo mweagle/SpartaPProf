74508090434204098b75162a7792327b00e0fd06
Matt Weagle <mweagle@gmail.com>
2018-10-20 06:16:59 -0700

    Minor updates

--- pprof/spartaPProf.go
+++ pprof/spartaPProf.go
@@ -3,23 +3,24 @@
 package pprof
 
 import (
+	"context"
+	"log"
+	"os"
 	"time"
 
 	sparta "github.com/mweagle/Sparta"
-	ssmcache "github.com/mweagle/ssm-cache"
-	"github.com/sirupsen/logrus"
 )
-
 
 // LogRelayFunction is the mock relay function that ships
 // logs to an external service. In the default case it's a nop.
-func LogRelayFunction(ctx context.Context) error
+func LogRelayFunction(ctx context.Context) error {
 	return nil
 }
 
 // InitializeProfiler registers the profiler
 func InitializeProfiler() {
-	log.Info("Attempting to register Sparta profiler")
+	logger := log.New(os.Stdout, "logger: ", log.Llongfile|log.LUTC)
+	logger.Print("Initializing Sparta profiler from SSM keys")
 
 	// Install the profiling hook. During `provision`, this will annotate
 	// each lambda function with enough context to publish profile snapshots
